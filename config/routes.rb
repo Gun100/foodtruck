dda6bf6d749e7c46bedc122216bf3ba345b3af8d
Lee gun woo <casio3612@gmail.com>
2018-07-07 08:29:33 +0000

    modify route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   resources :reviews
   resources :trucks
   devise_for :users
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
