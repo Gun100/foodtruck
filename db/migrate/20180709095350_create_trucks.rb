e053630c21a3b3f81b3bb916dbc23621e2911956
Lee gun woo <casio3612@gmail.com>
2018-07-09 10:06:20 +0000

    add table association

--- db/migrate/20180709095350_create_trucks.rb
+++ db/migrate/20180709095350_create_trucks.rb
@@ -1,7 +1,11 @@
 class CreateTrucks < ActiveRecord::Migration[5.1]
   def change
     create_table :trucks do |t|
-
+      t.string :truck_name
+      t.float :truck_x
+      t.float :truck_y
+      t.belongs_to :user
+      
       t.timestamps
     end
   end
